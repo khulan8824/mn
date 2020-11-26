d67227188ad4f58c11ce3c6fec287f233b979a70
khulan8824 <khulan.msc.2012@gmail.com>
2020-11-26 17:58:41 +0100

    bas

--- delay.sh
+++ delay.sh
@@ -4,9 +4,9 @@
 interface=ens18
 ip=(10.139.40.206 10.139.40.207 10.139.40.208 10.139.40.209 10.139.40.210 10.139.40.211 10.139.40.212 10.139.40.13 10.139.40.214 )
 delay=3ms
+tc qdisc add dev $interface root handle 1: prio
 for i in "${ip[@]}"
 do
-    tc qdisc add dev $interface root handle 2: prio
     tc filter add dev $interface parent 1:0 protocol ip prio 1 u32 match ip dst $i flowid 2:1
     tc qdisc add dev $interface parent 1:1 handle 2: netem delay $delay
 done
