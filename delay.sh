c59b4a0691e4a24eab0ea3e27daa75beeb3b1378
khulan8824 <khulan.msc.2012@gmail.com>
2020-11-26 17:55:43 +0100

    bas

--- delay.sh
+++ delay.sh
@@ -6,7 +6,7 @@ ip=(10.139.40.206 10.139.40.207 10.139.40.208 10.139.40.209 10.139.40.210 10.139
 delay=3ms
 for i in "${ip[@]}"
 do
-    tc qdisc add dev $interface root handle 1: prio
+    tc qdisc add dev $interface root handle 2: prio
     tc filter add dev $interface parent 1:0 protocol ip prio 1 u32 match ip dst $i flowid 2:1
     tc qdisc add dev $interface parent 1:1 handle 2: netem delay $delay
 done
@@ -16,6 +16,6 @@ delay=5ms
 for i in "${ip[@]}"
 do
     #tc qdisc add dev $interface root handle 1: prio
-    tc filter add dev $interface parent 1:0 protocol ip prio 1 u32 match ip dst $i flowid 2:1
-    tc qdisc add dev $interface parent 1:1 handle 2: netem delay $delay
+    tc filter add dev $interface parent 2:0 protocol ip prio 1 u32 match ip dst $i flowid 2:1
+    tc qdisc add dev $interface parent 2:1 handle 2: netem delay $delay
 done
