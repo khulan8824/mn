e1b2791a3f1466c7265f8010feb5349054fc661c
khulan8824 <khulan.msc.2012@gmail.com>
2020-11-27 10:51:15 +0000

    Added delay

--- delay.sh
+++ delay.sh
@@ -1,36 +1,31 @@
 #
 #!/bin/bash
 
-interface=ens18
-ip=(10.139.40.206 10.139.40.207 10.139.40.208 10.139.40.209 10.139.40.210 10.139.40.211 10.139.40.212 10.139.40.13 10.139.40.214 )
-delay=3ms
-tc qdisc add dev $interface root handle 1: prio
-tc class add dev $interace parent 1: classid 1:1 prio
-tc class add dev $interface parent 1:1 classid 1:11 prio netem delay 3ms
-tc class add dev $interface parent 1:1 classid 1:12 prio netem delay 5ms
+tc qdisc add dev ens18 root handle 1: prio
+tc filter add dev ens18 protocol ip parent 1: prio 1 u32 match ip dst 10.139.40.206 flowid 1:1
+tc filter add dev ens18 protocol ip parent 1: prio 1 u32 match ip dst 10.139.40.207 flowid 1:1
+tc filter add dev ens18 protocol ip parent 1: prio 1 u32 match ip dst 10.139.40.208 flowid 1:1
+tc filter add dev ens18 protocol ip parent 1: prio 1 u32 match ip dst 10.139.40.209 flowid 1:1
+tc filter add dev ens18 protocol ip parent 1: prio 1 u32 match ip dst 10.139.40.210 flowid 1:1
+tc filter add dev ens18 protocol ip parent 1: prio 1 u32 match ip dst 10.139.40.211 flowid 1:1
+tc filter add dev ens18 protocol ip parent 1: prio 1 u32 match ip dst 10.139.40.212 flowid 1:1
+tc filter add dev ens18 protocol ip parent 1: prio 1 u32 match ip dst 10.139.40.213 flowid 1:1
+tc filter add dev ens18 protocol ip parent 1: prio 1 u32 match ip dst 10.139.40.214 flowid 1:1
 
 
-tc filter add dev $interface parent 1:11 protocol ip prio 1 u32 match ip dst 10.139.40.206 flowid 1:11
-tc filter add dev $interface parent 1:12 protocol ip prio 1 u32 match ip dst 10.139.40.215 flowid 1:12
+tc filter add dev ens18 protocol ip parent 1: prio 3 u32 match ip dst 10.139.40.215 flowid 1:3
+tc filter add dev ens18 protocol ip parent 1: prio 3 u32 match ip dst 10.139.40.216 flowid 1:3
+tc filter add dev ens18 protocol ip parent 1: prio 3 u32 match ip dst 10.139.40.217 flowid 1:3
+tc filter add dev ens18 protocol ip parent 1: prio 3 u32 match ip dst 10.139.40.218 flowid 1:3
+tc filter add dev ens18 protocol ip parent 1: prio 3 u32 match ip dst 10.139.40.219 flowid 1:3
+tc filter add dev ens18 protocol ip parent 1: prio 3 u32 match ip dst 10.139.40.220 flowid 1:3
 
-tc qdisc add dev $interface parent 1:11 handle 10: netem delay 3ms
-tc qdisc add dev $interface parent 1:12 handle 20: netem delay 5ms
+tc filter add dev ens18 protocol ip parent 1: prio 2 u32 match ip dst 10.139.40.201 flowid 1:2
+tc filter add dev ens18 protocol ip parent 1: prio 2 u32 match ip dst 10.139.40.202 flowid 1:2
+tc filter add dev ens18 protocol ip parent 1: prio 2 u32 match ip dst 10.139.40.203 flowid 1:2
+tc filter add dev ens18 protocol ip parent 1: prio 2 u32 match ip dst 10.139.40.204 flowid 1:2
+tc filter add dev ens18 protocol ip parent 1: prio 2 u32 match ip dst 10.139.40.205 flowid 1:2
 
-
-#tc filter add dev $interface parent 1:0 protocol ip prio 1 u32 match ip dst 10.139.40.206 flowid 2:1
-#tc qdisc add dev $interface parent 1:1 handle 2: netem delay $delay
-#done
-
-#ip=(10.139.40.215 10.139.40.216 10.139.40.217 10.139.40.218 10.139.40.219 10.139.40.220)
-
-
-#tc qdisc add dev eth1 handle 1: root htb r2q 1700
-
-#tc class add dev eth1 parent 1: classid 1:1 htb rate 100Mbps ceil 100Mbps
-#tc class add dev eth1 parent 1:1 classid 1:11 htb rate 100Mbps
-#tc class add dev eth1 parent 1:1 classid 1:12 htb rate 100Mbps
-
-#tc filter add dev eth1 parent 1: protocol ip prio 1 u32 match ip src 10.0.1.0/24 flowid 1:11
-#tc filter add dev eth1 parent 1: protocol ip prio 1 u32 match ip src 20 10.0.2.0/24 flowid 1:12
-#tc qdisc add dev eth1 parent 1:11 handle 10: netem trace testpattern1.bin 0 1
-#tc qdisc add dev eth1 parent 1:12 handle 20: netem trace testpattern2.bin 100 0
+tc qdisc add dev ens18 parent 1:1 handle 10: netem delay 3ms
+tc qdisc add dev ens18 parent 1:3 handle 30: netem delay 5ms
+tc qdisc add dev ens18 parent 1:2 handle 20: netem delay 0ms
